7a485c865bac6044c916d69ddf5aaf3849e78d14
PeteTheGamer962 <162204327+PeteTheGamer962real123@users.noreply.github.com>
2024-08-13 13:18:43 -0500

    Add files via upload

---
+++ CppThirdPersonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CppThirdPersonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPTHIRDPERSON_API ACppThirdPersonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ ThirdPersonPlayerCont.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThirdPersonPlayerCont.h"
+#include "ThirdPersonPlayerCamManager.h"
+
